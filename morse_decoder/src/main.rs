105e8c07da7c9e4f5654f077bd46a290542e6762
Damon Marc Rocha <dmarcr1997@gmail.com>
2020-06-24 11:45:35 -0500

    finished user input loop

--- morse_decoder/src/main.rs
+++ morse_decoder/src/main.rs
@@ -150,24 +150,32 @@ fn main() {
     
     println!("Hello Welcome to No Re Morse, Just kidding this is a Morse Code translator/decoder");
     println!("----------------------------------------------------------------------------------");
-    println!("Please enter morse code with ONE space between letters and TWO space between words");
+    println!("Please enter morse code with ONE space between letters and TWO space between words\n(. - .--.  . - .--. = ETP ETP)");
     println!("Please enter words with ONE space between them and in all capital letters");
-    println!("----------------------------------------------------------------------------------");
-    println!("Enter decode to translate from morse code or encode to translate to morse code:   ");
-    println!("----------------------------------------------------------------------------------");
-    let mut code_choice = String::new();
-    io::stdin()
-        .read_line(&mut code_choice)
-        .expect("Failed to read line");
-    let code_choice = code_choice.trim().to_string();
-    println!("{}",code_translator(code_choice));
-    
-    
-    
-    // println!("{}", decoder.decode_morse("-.. .- -- --- -."));
-    // println!("{}", decoder.convert_to_morse("DAMON"));
-    // println!("{}", decoder.decode_morse(&decoder.convert_to_morse("HELLO WORLD")));
-    // println!("{}", decoder.decode_morse(".... . .-.. .-.. ---  .-- --- .-. .-.. -.."));
+    loop{
+        println!("----------------------------------------------------------------------------------");
+        println!("Enter decode to translate from morse code or encode to translate to morse code:   ");
+        println!("----------------------------------------------------------------------------------");
+        let mut code_choice = String::new();
+        io::stdin()
+            .read_line(&mut code_choice)
+            .expect("Failed to read line");
+        let code_choice = code_choice.trim().to_string();
+        println!("New Message: {}",code_translator(code_choice));
+
+        println!("Run Again(Y?N): ");
+        let mut choice = String::new(); 
+        io::stdin()
+            .read_line(&mut choice)
+            .expect("Failed to read line");
+        let choice = choice.trim().to_string();
+        if choice.to_uppercase() != String::from("Y"){
+            break;
+        }
+    }
+        
+    println!("Well Thanks For Using My CLI application");
+    println!("If You Want to help make this project better contribute here: https://github.com/dmarcr1997/rust_morse_code_decoder_cli");
 }
 
 
